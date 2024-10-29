d6d0ddba7dd7e1aaf4673d71ac03bc9ba788f776
kayro <kayro@onfly.com.br>
2024-10-28 23:30:48 -0300

    Limpando

--- PrimAlgorithm/Prim.py
+++ PrimAlgorithm/Prim.py
@@ -9,9 +9,13 @@ graph_lista = {
     "v8": {"v1": 0, "v2": 5, "v3": 0, "v4": 0, "v5": 4, "v6": 0, "v7": 2, "v8": 0, "v9": 4},
     "v9": {"v1": 5, "v2": 3, "v3": 0, "v4": 0, "v5": 1, "v6": 0, "v7": 0, "v8": 4, "v9": 0}
 }
-
-def filterAdjacencia(adjacencia):
-    return {k: v for k, v in adjacencia.items() if v != 0}
+graph_dict = {
+    "v1": {"v1": 0, "v2": 2, "v3": 3, "v4": 4, "v5": 3},
+              "v2": {"v1": 1, "v2": 0, "v3": 4, "v4": 2, "v5": 2},
+              "v3": {"v1": 2, "v2": 6, "v3": 0, "v4": 3, "v5": 4},
+              "v4": {"v1": 3, "v2": 5, "v3": 2, "v4": 0, "v5": 2},
+              "v5": {"v1": 3, "v2": 5, "v3": 2, "v4": 4, "v5": 0},
+              }
 def getMenorPeso(fila,visitadosList):
     menorPeso = float('inf')
     verticeMenorPeso = ''
@@ -21,12 +25,6 @@ def getMenorPeso(fila,visitadosList):
             verticeMenorPeso = vertice
     return verticeMenorPeso
 
-graph_dict = {"v1": {"v1": 0, "v2": 2, "v3": 3, "v4": 4, "v5": 3},
-              "v2": {"v1": 1, "v2": 0, "v3": 4, "v4": 2, "v5": 2},
-              "v3": {"v1": 2, "v2": 6, "v3": 0, "v4": 3, "v5": 4},
-              "v4": {"v1": 3, "v2": 5, "v3": 2, "v4": 0, "v5": 2},
-              "v5": {"v1": 3, "v2": 5, "v3": 2, "v4": 4, "v5": 0},
-              }
 priorityQueue = {}
 for vert in graph_lista:
     vertice = graph_lista[vert]
@@ -54,13 +52,13 @@ while len(priorityQueue) > 0:
             priorityQueue[adjacente]['peso'] = adjacentesMenorPeso[adjacente]
 
 print(arvore)
-
-{'v1': {'peso': 0, 'anterior': 0},
- 'v2': {'peso': 1, 'anterior': 'v1'},
- 'v3': {'peso': 2, 'anterior': 'v2'},
- 'v5': {'peso': 3, 'anterior': 'v2'},
- 'v6': {'peso': 1, 'anterior': 'v5'},
- 'v9': {'peso': 1, 'anterior': 'v5'},
- 'v4': {'peso': 2, 'anterior': 'v5'},
- 'v7': {'peso': 3, 'anterior': 'v4'},
- 'v8': {'peso': 2, 'anterior': 'v7'}}
+# resultado:
+# {'v1': {'peso': 0, 'anterior': 0},
+#  'v2': {'peso': 1, 'anterior': 'v1'},
+#  'v3': {'peso': 2, 'anterior': 'v2'},
+#  'v5': {'peso': 3, 'anterior': 'v2'},
+#  'v6': {'peso': 1, 'anterior': 'v5'},
+#  'v9': {'peso': 1, 'anterior': 'v5'},
+#  'v4': {'peso': 2, 'anterior': 'v5'},
+#  'v7': {'peso': 3, 'anterior': 'v4'},
+#  'v8': {'peso': 2, 'anterior': 'v7'}}
